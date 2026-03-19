3a188334866eca3d8c7d98bec607a06a883e6cbe
Devikaa Dinesh <devikaapreethi2000@gmail.com.com>
2026-03-18 21:17:38 -0400

    Added a eda file

---
+++ EDA.sql
@@ -0,0 +1,44 @@
+-- EDA
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoff_staging2;
+
+SELECT company, SUM(total_laid_off)
+FROM layoff_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
+FROM layoff_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC;
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_laid
+FROM layoff_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC)
+SELECT `MONTH` , SUM(total_laid) OVER(ORDER BY `MONTH`)
+FROM Rolling_Total;
+
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoff_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY company ASC;
+
+WITH Company_Year(company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoff_staging2
+GROUP BY company, YEAR(`date`)
+), Company_Year_Rank AS
+(SELECT *, DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking 
+FROM Company_Year
+WHERE years  IS NOT NULL)
+SELECT * 
+FROM Company_Year_Rank
+WHERE Ranking <=5;
